d6ea420da5bd7f46ee5093b758ed96c8d48b880e
Isaque <isaquecoelho.dj@gmail.com>
2025-11-14 23:19:52 -0300

    feat: adicionar properties e validações nos modelos

--- models/categoria.py
+++ models/categoria.py
@@ -1,13 +1,31 @@
 class Categoria:
-  """define as regras de peso, faixa e idade para uma divisao"""
   def __init__(self, nome, faixa_min, peso_max_kg, idade_min=18, idade_max=99):
-    #o id da categoria pode ser um contador ou um nome simplificado
     self.categoria_id = nome.replace(" ", "_").lower()
-    self.nome = nome
-    self.faixa_min = faixa_min
-    self.peso_max_kg = peso_max_kg
-    self.idade_min = idade_min
-    self.idade_max = idade_max
-  def __str__(self):
+    self._nome = nome.strip()
+    self._faixa_min = faixa_min.strip()
+    self._peso_max_kg = float(peso_max_kg)
+    self._idade_min = int(idade_min)
+    self._idade_max = int(idade_max)
 
+  @property
+  def nome(self):
+    return self._nome
+
+  @property
+  def faixa_min(self):
+    return self._faixa_min
+
+  @property
+  def peso_max_kg(self):
+    return self._peso_max_kg
+
+  @property
+  def idade_min(self):
+    return self._idade_min
+
+  @property
+  def idade_max(self):
+    return self._idade_max
+
+  def __str__(self):
     return f"Categoria: {self.nome} ({self.faixa_min} | até {self.peso_max_kg}kg)"
